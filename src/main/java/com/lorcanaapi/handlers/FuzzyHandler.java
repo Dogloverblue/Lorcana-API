85cd0e61ec74af64130f2828a3d79100b9302f8c
Dogloverblue <lowenjacoba2@gmail.com>
2023-08-13 11:23:11 -0700

    Make /fuzzy/ ignore non exact respones

--- src/main/java/com/lorcanaapi/handlers/FuzzyHandler.java
+++ src/main/java/com/lorcanaapi/handlers/FuzzyHandler.java
@@ -5,6 +5,9 @@ import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
@@ -14,7 +17,7 @@ import me.xdrop.fuzzywuzzy.model.ExtractedResult;
 public class FuzzyHandler implements HttpHandler {
 		static int FuzzyRequestCount = 0;
 	    String response = "sd";
-	    static HashMap<String, String> data;
+	    static HashMap<String, String> data = new HashMap<String, String>();
       @Override
       public void handle(HttpExchange t) throws IOException {
 		 System.out.println("Fuzzy response made; it was fuzzy request number " + FuzzyRequestCount);
@@ -28,7 +31,16 @@ public class FuzzyHandler implements HttpHandler {
           os.close();
       }
       public FuzzyHandler(HashMap<String, String> cardData) {
-    	  data = cardData;
+    	  for (String key: cardData.keySet()) {
+    		 String value = cardData.get(key);
+    		 JSONObject jo = new JSONObject(value);
+    		 try {
+    			 jo.getString("name");
+    			 data.put(key, value);
+    		 } catch (JSONException e) {
+				System.out.println("Not adding " + key + " to /fuzzy/ because it's a non exact response");
+			}
+    	  }
       }
      
 	    
